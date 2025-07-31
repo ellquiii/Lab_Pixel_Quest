a0fccaeb27e67904a3feeafbd29cbe29019f68b3
Ellyza <ellyzarya@gmail.com>
2025-07-31 09:54:34 -0400

    FixedFadeIn
    Fixed the fade in and fade out for next scenes

--- Assets/HB_SCRIPTS/SceneChanger.cs
+++ Assets/HB_SCRIPTS/SceneChanger.cs
@@ -1,82 +1,96 @@
-using System.Collections;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class SceneChanger : MonoBehaviour
 {
-    enum FadeStatus
+    private enum FadeStatus
     {
-        fading_id,
-        fading_out,
-        none
+        FadingIn,
+        FadingOut,
+        None
     }
 
     public static SceneChanger Instance;
+
+    [Header("Fade Settings")]
     public Image fadeImage;
-    public float fadeDuration;
+    public float fadeDuration = 1f;
 
-    private FadeStatus currentFadeStatus = FadeStatus.none;
-    private float fadeTimer;
-    private string sceneToLoad;
+    private FadeStatus currentFadeStatus = FadeStatus.None;
+    private float fadeTimer = 0f;
+    private string sceneToLoad = "";
 
-    void Start()
+    void Awake()
     {
         if (Instance == null)
         {
             Instance = this;
-
+            DontDestroyOnLoad(gameObject);
             SceneManager.sceneLoaded += OnSceneLoaded;
         }
         else
-            Destroy(gameObject);
-
-        DontDestroyOnLoad(gameObject);
-    }
-
-
-    void OnSceneLoaded(Scene scene, LoadSceneMode mode)
-    {
-        //scene loaded, running fade-in
-        currentFadeStatus = FadeStatus.fading_id;
-    }
-
-    public void ChangeScene(string _name)
-    {
-        sceneToLoad = _name;
-        currentFadeStatus = FadeStatus.fading_out;
-    }
-
-    void Update()
-    {
-        if (currentFadeStatus != FadeStatus.none)
         {
-            fadeTimer += Time.deltaTime;
+            Destroy(gameObject);
+        }
+    }
 
-            if (fadeTimer > fadeDuration) //done fading
+    private void Start()
+    {
+        // Start with a full black screen, then fade in
+        fadeImage.color = Color.black;
+        currentFadeStatus = FadeStatus.FadingIn;
+        fadeTimer = 0f;
+    }
+
+    private void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+    {
+        // Start fading in after the new scene loads
+        fadeTimer = 0f;
+        currentFadeStatus = FadeStatus.FadingIn;
+    }
+
+    public void ChangeScene(string sceneName)
+    {
+        if (currentFadeStatus == FadeStatus.None)
+        {
+            sceneToLoad = sceneName;
+            fadeTimer = 0f;
+            currentFadeStatus = FadeStatus.FadingOut;
+        }
+    }
+
+    private void Update()
+    {
+        if (currentFadeStatus == FadeStatus.None)
+            return;
+
+        fadeTimer += Time.deltaTime;
+        float progress = fadeTimer / fadeDuration;
+
+        if (progress >= 1f)
+        {
+            if (currentFadeStatus == FadeStatus.FadingOut)
             {
-                fadeTimer = 0;
-
-                if (currentFadeStatus == FadeStatus.fading_out)
-                {
-                    SceneManager.LoadScene(sceneToLoad);
-                    fadeImage.color = Color.black;
-                }
-                else
-                    fadeImage.color = Color.clear;
-
-                currentFadeStatus = FadeStatus.none;
+                fadeImage.color = Color.black;
+                SceneManager.LoadScene(sceneToLoad);
+                // Fade-in will be triggered by OnSceneLoaded
             }
-            else //still fading
+            else if (currentFadeStatus == FadeStatus.FadingIn)
             {
-                float alpha = 0;
-                if (currentFadeStatus == FadeStatus.fading_out)
-                    alpha = Mathf.Lerp(0, 1, fadeTimer / fadeDuration);
-                else
-                    alpha = Mathf.Lerp(1, 0, fadeTimer / fadeDuration);
-
-                fadeImage.color = new Color(0, 0, 0, alpha);
+                fadeImage.color = Color.clear;
+                currentFadeStatus = FadeStatus.None;
             }
+
+            fadeTimer = 0f;
+        }
+        else
+        {
+            float alpha = (currentFadeStatus == FadeStatus.FadingOut)
+                ? Mathf.Lerp(0f, 1f, progress)
+                : Mathf.Lerp(1f, 0f, progress);
+
+            fadeImage.color = new Color(0f, 0f, 0f, alpha);
         }
     }
 }
